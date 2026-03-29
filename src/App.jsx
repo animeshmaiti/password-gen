70eff06e29348467a3154e42c3b9580698277046
animeshmaiti <a.maiti770@gmail.com>
2026-03-29 13:07:26 +0530

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,20 @@
-import { useState, useEffect } from "react";
-import PasswordDisplay from "./components/PasswordDisplay";
-import PasswordOptions from "./components/PasswordOptions";
-import GenerateButton from "./components/GenerateButton";
-import StrengthIndicator from "./components/StrengthIndicator";
-import { generatePassword, getStrength } from "./utils/passwordUtils";
+import Basic from "./components/pages/Basic";
+import Advanced from "./components/pages/Advanced";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [length, setLength] = useState(16);
-  const [password, setPassword] = useState("");
-
-  const [options, setOptions] = useState({
-    lowercase: true,
-    uppercase: true,
-    numbers: true,
-    symbols: true,
-    excludeSimilar: false,
-    excludeAmbiguous: false,
-  });
-  const isValid = Object.values(options)
-    .slice(0, 4) // only first 4 (actual character types)
-    .some((v) => v === true);
-
-  const handleGenerate = () => {
-    if (!isValid) {
-      alert("Please select at least one character type!");
-      return;
-    }
-    const pass = generatePassword(length, options);
-    setPassword(pass);
-  };
-  useEffect(() => {
-    handleGenerate();
-  }, [length, options]);
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center items-center p-4">
-      <div className="bg-white shadow-xl rounded-2xl w-full max-w-md p-6 space-y-4">
-        <h1 className="text-xl font-bold">Strong Password Generator</h1>
-
-        <PasswordDisplay password={password} setPassword={setPassword} />
-
-        <StrengthIndicator strength={getStrength(length)} />
-
-        <PasswordOptions
-          length={length}
-          setLength={setLength}
-          options={options}
-          setOptions={setOptions}
-        />
-
-        <GenerateButton onGenerate={handleGenerate} />
-      </div>
+    <div className="max-w-md mx-auto p-4 space-y-4">
+      {/* Tabs (Navbar) */}
+      <NavBar/>
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Basic />} />
+        <Route path="/advanced" element={<Advanced />} />
+      </Routes>
     </div>
   );
 }
