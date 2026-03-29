4ab8df2927a2a3bdf81cd25e0f14475716ec6ffb
animeshmaiti <a.maiti770@gmail.com>
2026-03-29 11:26:06 +0530

    ensure strong password

--- src/utils/passwordUtils.js
+++ src/utils/passwordUtils.js
@@ -7,37 +7,46 @@ export const charSets = {
 
 const similarChars = "0oO1lI";
 const ambiguousChars = "~,;:.{}<>[]()/\\'`";
+const randomChar = (str) => str[Math.floor(Math.random() * str.length)];
+const filter = (str, remove) =>
+  str
+    .split("")
+    .filter((c) => !remove.includes(c))
+    .join("");
+const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
 
 export const generatePassword = (length, options) => {
   let chars = "";
+  let required = [];
 
-  Object.keys(options).forEach((key) => {
-    if (options[key] && charSets[key]) {
-      chars += charSets[key];
-    }
-  });
-
-  if (options.excludeSimilar) {
-    chars = chars
-      .split("")
-      .filter((c) => !similarChars.includes(c))
-      .join("");
+  if (options.lowercase) {
+    chars += charSets.lowercase;
+    required.push(randomChar(charSets.lowercase));
   }
-  if (options.excludeAmbiguous) {
-    chars = chars
-      .split("")
-      .filter((c) => !ambiguousChars.includes(c))
-      .join("");
+  if (options.uppercase) {
+    chars += charSets.uppercase;
+    required.push(randomChar(charSets.uppercase));
   }
-
-  if (!chars) return "";
-
-  let pass = "";
-  for (let i = 0; i < length; i++) {
-    pass += chars[Math.floor(Math.random() * chars.length)];
+  if (options.numbers) {
+    chars += charSets.numbers;
+    required.push(randomChar(charSets.numbers));
+  }
+  if (options.symbols) {
+    chars += charSets.symbols;
+    required.push(randomChar(charSets.symbols));
   }
 
-  return pass;
+  // filters
+  if (options.excludeSimilar) chars = filter(chars, similarChars);
+  if (options.excludeAmbiguous) chars = filter(chars, ambiguousChars);
+
+  let pass = [...required];
+
+  for (let i = pass.length; i < length; i++) {
+    pass.push(randomChar(chars));
+  }
+
+  return shuffle(pass).join("");
 };
 
 export const getStrength = (length) => {
