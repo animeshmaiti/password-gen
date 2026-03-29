88a09b1eac1cffb2c29d1630f8ccde788a24ff4d
animeshmaiti <a.maiti770@gmail.com>
2026-03-29 11:24:50 +0530

    slider

--- src/components/PasswordOptions.jsx
+++ src/components/PasswordOptions.jsx
@@ -9,15 +9,19 @@ const PasswordOptions = ({ length, setLength, options, setOptions }) => {
   };
   return (
     <div className="space-y-3">
-      <div>
-        <label className="block text-sm font-medium">Password Length</label>
-        <input
-          type="number"
-          value={length}
-          onChange={(e) => setLength(Number(e.target.value))}
-          className="w-full border rounded-lg p-2"
-        />
+      <div className="flex justify-between text-sm font-medium">
+        <span>Password Length</span>
+        <span>{length}</span>
       </div>
+
+      <input
+        type="range"
+        min="4"
+        max="32"
+        value={length}
+        onChange={(e) => setLength(Number(e.target.value))}
+        className="w-full"
+      />
 
       {Object.keys(options).map((key) => (
         <label key={key} className="flex items-center gap-2">
